0de946077f7b2341894c1e10baa22acbbaa5df3a
muhammad-jamal-bek <muhammad-jamal-bek@hotmail.com>
2021-07-08 12:26:33 +0300

    Completed the Landing Page

---
+++ marwaphotography/src/Welcome/Weclome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Welcome.css";
+
+const welcome= () =>{
+    return (
+    <body>
+        <div className="container">
+            <a className="owner" href="#">Marwa Hodeib</a>
+        </div>
+    </body>
+    )}
+
+export default welcome;
