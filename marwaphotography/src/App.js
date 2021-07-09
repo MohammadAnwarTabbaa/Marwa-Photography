2707ba49cb7c6f4ac5078956bc1680523b475b01
muhammad-jamal-bek <muhammad-jamal-bek@hotmail.com>
2021-07-09 21:56:28 +0300

    Added the 'About me' Page

--- marwaphotography/src/App.js
+++ marwaphotography/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component} from 'react';
 import './App.css';
 import Welcome from './Welcome/Weclome';
+import About from './About/About'
 
 class App extends Component{
   render(){
   return (
     <div className="App">
-      <Welcome />
+      <About />
     </div>
   );
 }
